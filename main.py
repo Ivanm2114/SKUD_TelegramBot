f4cb1e8fdb2973b1bcc00ed3335023bfb8be4c02
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2024-03-30 11:53:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# https://habr.com/ru/articles/697052/
+
+from dotenv import load_dotenv
+import os
+from telebot import types
+
+import telebot
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup=markup)
+
+
+bot.infinity_polling()
