9ebadf395e0dc85a0ab93269f633a554aef8197c
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2024-04-01 12:13:44 +0300

    v0.01

--- main.py
+++ main.py
@@ -5,25 +5,39 @@ import os
 from telebot import types
 
 import telebot
+import tinytuya
+import time
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+d = tinytuya.OutletDevice('bf45653c868eec068bawzu', '192.168.10.10', '*BVvpBw1JTtz1ky.')
+d.set_version(3.3)
 
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    btn2 = types.KeyboardButton("Получить доступ к комнате")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id,
+                     text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(
+                         message.from_user), reply_markup=markup)
 
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup=markup)
+    if message.text == 'Получить доступ к комнате':
+        print('Access granted')
+        payload = d.generate_payload(tinytuya.CONTROL, {'1': 'AgAAAAMAZAEAAAE='})
+        print(payload)
+        d._send_receive(payload)
+        bot.send_message(message.chat.id, text="Ты получил доступ")
+    else:
+        bot.send_message(message.chat.id, text='Что-то не то')
 
 
 bot.infinity_polling()
