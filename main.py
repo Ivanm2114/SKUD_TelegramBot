8bf92e4f8fcbf23cb277e5e3cee427e55731703d
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2024-04-13 13:48:54 +0300

    multilock version

--- main.py
+++ main.py
@@ -4,131 +4,108 @@ from dotenv import load_dotenv
 import os
 from telebot import types
 
+from Lock import Lock
 import telebot
 import tinytuya
 import time
 import json
 
+locks = {'435': Lock('435', 'bf45653c868eec068bawzu',
+                     '192.168.137.95', "'sSfG?_+ojZbW+J^"),
+         '505': Lock('505', 'bfc2362c23fa6fe8a32qxv',
+                     '192.168.137.146', "?mcc<FabE]py;ViN")}
+cur_lock = ''
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-users = {
-    'users': {}}
+users = {}
+
+def save_users():
+    global users
+    with open('users.json', 'w') as f:
+        json.dump(users, f, indent=4)
+
 
 if 'users.json' in os.listdir():
     with open('users.json') as f:
         users = json.load(f)
 else:
     file = open('users.json', 'w')
+    json.dump(users, file)
     file.close()
+
+for room in locks.keys():
+    if room not in users.keys():
+        t = {}
+        for user in users[list(locks.keys())[0]]:
+            t[user] = {}
+        users[room] = t
+        save_users()
+    locks[room].sync_users()
 print(users)
 bot = telebot.TeleBot(BOT_TOKEN)
 print('BOT STARTED')
-d = tinytuya.OutletDevice('bf45653c868eec068bawzu', '192.168.10.8', 'U_tn`Qb%*Rrm#/f_')
-d.set_version(3.3)
 
 
 def get_start_menu_markup():
     reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn = types.KeyboardButton("Настроить доступ к комнате")
-    reply_markup.add(btn)
+    for room in locks.keys():
+        reply_markup.row(types.KeyboardButton(f"Настроить доступ к комнате {room}"))
     return reply_markup
+
+
 
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.send_message(message.chat.id, text="Привет", reply_markup=get_start_menu_markup())
-    users['users'][str(message.from_user.id)] = {}
-    with open('users.json', 'w') as f:
-        json.dump(users, f, indent=4)
+    for room in locks.keys():
+        if str(message.from_user.id) not in locks[room].get_users().keys():
+            locks[room].add_user(str(message.from_user.id))
+            users[room][str(message.from_user.id)] = {}
+    save_users()
 
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    if message.text == 'Настроить доступ к комнате':
+    global cur_lock
+    if 'Настроить доступ к комнате' in message.text:
+        room = message.text.split()[-1]
+        cur_lock = locks[room]
+        lock_users = cur_lock.get_users()
+        uid = str(message.from_user.id)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        if str(message.from_user.id) in list(users['users'].keys()):
-            if 'card' in users['users'][str(message.from_user.id)].keys():
+        if uid in list(lock_users.keys()):
+            if 'card' in lock_users[uid].keys():
                 KeyBtn = types.KeyboardButton("Удалить пропуск из базы замка")
             else:
                 KeyBtn = types.KeyboardButton("Добавить пропуск в базу замка")
-            if 'fingerprint' in users['users'][str(message.from_user.id)].keys():
+            if 'fingerprint' in lock_users[uid].keys():
                 FingerBtn = types.KeyboardButton("Удалить отпечаток из базы замка")
             else:
                 FingerBtn = types.KeyboardButton("Добавить отпечаток в базу замка")
         back_button = types.KeyboardButton("Назад")
-        markup.add(KeyBtn, FingerBtn, back_button)
+        markup.row(KeyBtn, FingerBtn)
+        markup.row(back_button)
         bot.send_message(message.chat.id, text="Выберите функцию", reply_markup=markup)
     elif message.text == "Добавить отпечаток в базу замка":
-        add_fingerprint(message)
+        if cur_lock.add_fingerprint(str(message.from_user.id)):
+            bot.send_message(message.chat.id, text="Вы добавили отпечаток пальца", reply_markup=get_start_menu_markup())
     elif message.text == 'Добавить пропуск в базу замка':
-        add_card(message)
+        if cur_lock.add_card(str(message.from_user.id)):
+            bot.send_message(message.chat.id, text="Вы добавили карту", reply_markup=get_start_menu_markup())
     elif message.text == "Удалить отпечаток из базы замка":
-        delete_fingerprint(message)
+        cur_lock.delete_fingerprint(str(message.from_user.id))
+        bot.send_message(message.chat.id, text="Вы удалили отпечаток пальца", reply_markup=get_start_menu_markup())
     elif message.text == 'Удалить пропуск из базы замка':
-        delete_card(message)
+        cur_lock.delete_card(str(message.from_user.id))
+        bot.send_message(message.chat.id, text="Вы удалили карту", reply_markup=get_start_menu_markup())
     elif message.text == 'Назад':
         bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=get_start_menu_markup())
+    users[cur_lock.get_room()][str(message.from_user.id)] = cur_lock.get_user(str(message.from_user.id))
+    save_users()
 
-
-def add_fingerprint(message):
-    payload = d.generate_payload(tinytuya.CONTROL, {'1': 'AwAAAAMAAAMAAAE='})
-    d._send_receive(payload)
-    counter = 0
-    while counter < 3:
-        data = d.receive()
-        if data:
-            if 'dps' in data.keys():
-                if '1' in data['dps'].keys():
-                    if 'A/w' in data['dps']['1']:
-                        counter += 1
-                    elif 'A/0' in data['dps']['1']:
-                        break
-    data = d.receive()
-    if 'A/8' in data['dps']['1']:
-        bot.send_message(message.chat.id, text="Ты добавил отпечаток")
-        users['users'][str(message.from_user.id)]['fingerprint'] = data['dps']['1'][8:10]
-        with open('users.json', 'w') as f:
-            json.dump(users, f, indent=4)
-        bot.send_message(message.chat.id, text="Отпечаток добавлен", reply_markup=get_start_menu_markup())
-    else:
-        bot.send_message(message.chat.id, text="Что-то пошло не так попробуй ещё раз")
-
-
-def add_card(message):
-    payload = d.generate_payload(tinytuya.CONTROL, {'1': 'AgAAAAMAZAEAAAE='})
-    d._send_receive(payload)
-    data = d.receive()
-    if 'Av8' in data['dps']['1']:
-        bot.send_message(message.chat.id, text="Ты добавил карту")
-        users['users'][str(message.from_user.id)]['card'] = data['dps']['1'][8:10]
-        with open('users.json', 'w') as f:
-            json.dump(users, f, indent=4)
-        bot.send_message(message.chat.id, text="Карта добавлена", reply_markup=get_start_menu_markup())
-    else:
-        bot.send_message(message.chat.id, text="Что-то пошло не так попробуй ещё раз")
-
-
-def delete_fingerprint(message):
-    payload = d.generate_payload(tinytuya.CONTROL,
-                                 {'2': f'AwAAAAMA{users["users"][str(message.from_user.id)]["fingerprint"]}H/'})
-    print(payload)
-    d._send_receive(payload)
-    del users['users'][str(message.from_user.id)]['fingerprint']
-    with open('users.json', 'w') as f:
-        json.dump(users, f, indent=4)
-    bot.send_message(message.chat.id, text="Отпечаток удален", reply_markup=get_start_menu_markup())
-
-
-def delete_card(message):
-    payload = d.generate_payload(tinytuya.CONTROL,
-                                 {'2': f'AgAAAAMA{users["users"][str(message.from_user.id)]["card"]}H/'})
-    print(payload)
-    d._send_receive(payload)
-    del users['users'][str(message.from_user.id)]['card']
-    with open('users.json', 'w') as f:
-        json.dump(users, f, indent=4)
-    bot.send_message(message.chat.id, text="Карта удалена", reply_markup=get_start_menu_markup())
 
 bot.infinity_polling()
